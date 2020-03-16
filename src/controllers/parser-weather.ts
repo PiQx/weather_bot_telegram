bbff629f369ff98684977abf77bf3184b263ec62
rdruzhinin <r.druzhinin@tinkoff.ru>
2020-03-16 17:50:41 +0700

    fix: file to table with status weather to const

--- src/controllers/parser-weather.ts
+++ src/controllers/parser-weather.ts
@@ -2,10 +2,12 @@ import csv from 'csv-parser';
 import { createReadStream } from 'fs';
 import { TableWeather } from './table-weather';
 
+const PATH_TABLE = 'src/Multilingual_Weather_Conditions.csv';
+
 async function openTableWeather(): Promise<TableWeather[]> {
   const tableWeather: any = [];
   return new Promise((resolve, reject) => {
-    createReadStream('src/Multilingual_Weather_Conditions.csv')
+    createReadStream(PATH_TABLE)
       .pipe(csv())
       .on('data', data => tableWeather.push(data))
       .on('end', () => {
